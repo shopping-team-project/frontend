3088cc995e654e5eef1d32dc61343754e9ce5391
towerbreaker2200 <69230199+towerbreaker2200@users.noreply.github.com>
2023-09-22 23:35:25 +0900

    redux toolkit base

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Counter } from "./features/counter/Counter";
+
 function App() {
   return (
     <>
       <div className="text-3xl font-bold text-blue-600">base page is here!</div>
+      <Counter />
     </>
   );
 }
