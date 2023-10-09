af9425a5e20e5b40c93b1a162a9d6a221a318ab5
koowin42@gmail.com <koowin42@gmail.com>
2023-10-09 22:01:57 +0900

    Add states and logo component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,21 @@
-function Login() {
-  return <div></div>;
+import { ReactElement, useState } from "react";
+import Logo from "../components/Logo.tsx";
+
+function Login(): ReactElement {
+  const [id, setId] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLogin = () => {
+    //TODO: Implement
+  };
+
+  return (
+    <div className="flex-col">
+      <Logo />
+      <div>로그인</div>
+      <input type="text" placeholder="아이디(이메일)" />
+    </div>
+  );
 }
 
 export default Login;
